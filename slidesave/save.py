9bde841ad76f20b75fc3153fd6a3de30f71ea12b
Sam <tritoke@protonmail.com>
2019-07-23 22:22:03 +0100

    This should say where the backups are being saved so they are easier to find.

--- slidesave/save.py
+++ slidesave/save.py
@@ -2,12 +2,20 @@ import re
 from datetime import datetime
 from requests import get
 from time import sleep
+from os import getcwd
 
+
+BASEPATH = getcwd()
+print(f"\nSaving backups to {BASEPATH}\n")
 original_url = input("Enter page url: ")
 search = re.search(r"\S+/d/(?P<id>.*)/.*", original_url)
+
+
 if not search:
     print("Couldn't parse id from URL")
-    print("The URL should look like https://docs.google.com/presentation/d/<id>/edit/<garbage nobody cares about>")
+    print(
+        "The URL should look like https://docs.google.com/presentation/d/<id>/edit/<garbage nobody cares about>"
+    )
     print("If it doesn't look like this then you are probably on the wrong page.")
     slides_id = input("Enter ID manually: ")
 else:
@@ -15,7 +23,7 @@ else:
 url = f"https://docs.google.com/presentation/d/{slides_id}/export/pptx"
 request = get(url)
 if request:
-    with open(f"{datetime.strftime(datetime.now(),'%F--%H:%M')}.pptx","wb") as f:
+    with open(f"{BASEPATH}{datetime.strftime(datetime.now(),'%F--%H:%M')}.pptx", "wb") as f:
         f.write(request.content)
     while True:
         try:
@@ -25,7 +33,9 @@ if request:
             else:
                 break
         except ValueError:
-            print("Invalid number of minutes, please enter number and nothing else, i.e. 30")
+            print(
+                "Invalid number of minutes, please enter number and nothing else, i.e. 30"
+            )
     secs = mins * 60
     backups = 1
     start = datetime.now()
@@ -34,7 +44,7 @@ if request:
         backups += 1
         sleep(secs)
         request = get(url)
-        with open(f"{datetime.strftime(datetime.now(),'%F--%H:%M')}.pptx","wb") as f:
+        with open(f"{BASEPATH}{datetime.strftime(datetime.now(),'%F--%H:%M')}.pptx", "wb") as f:
             f.write(request.content)
 
 else:
