c40225e9371c36f5b697e37eb9dd1e390d0d0bbc
Sam <tritoke@protonmail.com>
2019-07-27 21:54:26 +0100

    Hopefully works now.

--- slidesave/save.py
+++ slidesave/save.py
@@ -1,53 +1,90 @@
 import re
 from datetime import datetime
-from requests import get
+from requests import get, head
 from time import sleep
-from os import getcwd
+import os
+
+BASEPATH = os.getcwd() + os.path.sep
 
 
-BASEPATH = getcwd() + "/"
-
-print(f"\nSaving backups to {BASEPATH}\n")
-
-original_url = input("Enter page url: ")
-search = re.search(r"\S+/d/(?P<id>[^/]*)/.*", original_url)
-
-if not search:
-    print("Couldn't parse id from URL")
-    print(
-        "The URL should look like https://docs.google.com/presentation/d/<id>/edit/<garbage nobody cares about>"
-    )
-    print("If it doesn't look like this then you are probably on the wrong page.")
-    slides_id = input("Enter ID manually: ")
-else:
-    slides_id = search.group("id")
-url = f"https://docs.google.com/presentation/d/{slides_id}/export/pptx"
-print(f"Downloading from {url}")
-request = get(url)
-if request:
-    with open(f"{BASEPATH}{datetime.strftime(datetime.now(),'%F-%H:%M.pptx')}", "wb") as f:
+def save(req):
+    """
+    Attempts to save the content from the supplied
+    request: req
+    """
+    print(f"Saving request of length {len(req.content)} bytes")
+    fname = datetime.strftime(datetime.now(), "%F_%H-%M")
+    with open(f"{BASEPATH}{fname}.pptx", "wb") as f:
         f.write(request.content)
+
+
+def parse_id(url):
+    """
+    Parses the file id from the slides URL
+    returns the ID
+    """
+
+    search = re.search(r"\S+/d/(?P<id>[^/]*)/.*", url)
+    if search:
+        return search.group(1)
+    else:
+        print("Failed to parse the ID from the URL")
+
+        slides_id = input("Enter the ID manually: ")
+
+        while not head(
+            f"https://docs.google.com/presentation/d/{slides_id}/export/pptx"
+        ):
+            print("Wrong ID, please try again below")
+            slides_id = input("Enter the ID manually: ")
+
+        return slides_id
+
+
+def get_backup_time():
+    """
+    Gets the number of minutes in between backups.
+    returns the number of seconds to sleep for between backups.
+    """
     while True:
         try:
-            mins = int(input("Enter the number of minutes between each backup save: "))
+            mins = float(
+                input("Enter the number of minutes between each backup save: ")
+            )
             if mins <= 0:
-                print("Invalid number of minutes, must be a positive integer.")
+                print("Invalid number of minutes, must be positive.")
             else:
                 break
         except ValueError:
             print(
                 "Invalid number of minutes, please enter number and nothing else, i.e. 30"
             )
-    secs = mins * 60
+    return int(mins * 60)
+
+
+print(f"\nSaving backups to {BASEPATH}\n")
+
+original_url = input("Enter page url: ")
+
+slides_id = parse_id(original_url)
+
+url = f"https://docs.google.com/presentation/d/{slides_id}/export/pptx"
+print(f"\nDownloading from {url}\n")
+
+request = get(url)
+
+if request:
+    save(request)
+
+    secs = get_backup_time()
+
     backups = 1
-    start = datetime.now()
+    start = datetime.strftime(datetime.now(), "%F %H:%M")
     while True:
         print(f"{backups} since {start}")
         backups += 1
         sleep(secs)
-        request = get(url)
-        with open(f"{BASEPATH}{datetime.strftime(datetime.now(),'%F-%H:%M.pptx')}", "wb") as f:
-            f.write(request.content)
+        save(get(url))
 
 else:
     print("Getting page failed, please restart and enter the correct id.")
